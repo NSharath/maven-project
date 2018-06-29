96319349e1f812445d517b0e44d8ef5645dd562e
NSharath <sharu300@gmail.com>
2018-06-28 19:51:26 -0700

    added cheeckstyle warnings revision v5

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,7 +14,7 @@ public Greeter() {
 /**
 * @param someone the name of a person
 * @return greeting string
-*/ 
+*/
 public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
